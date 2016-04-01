5849c75d310ac2c4dcb7e0070a992ad36faabf2f
Min Zhu <mzhu@financialengines.com>
2016-04-01 07:44:17 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,156 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  AlertIOS,
+  PushNotificationIOS,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+
+var Button = React.createClass({
+  render: function() {
+    return (
+      <TouchableHighlight
+        underlayColor={'white'}
+        style={styles.button}
+        onPress={this.props.onPress}>
+        <Text style={styles.buttonLabel}>
+          {this.props.label}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+});
+
+class iosPush2 extends React.Component {
+  componentWillMount() {
+    PushNotificationIOS.addEventListener('notification', this._onNotification);
+  }
+
+  componentWillUnmount() {
+    PushNotificationIOS.removeEventListener('notification', this._onNotification);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={this._sendNotification}
+          label="Send fake notification"
+        />
+      </View>
+    );
+  }
+
+  _sendNotification() {
+    require('RCTDeviceEventEmitter').emit('remoteNotificationReceived', {
+      aps: {
+        alert: 'Sample notification',
+        badge: '+1',
+        sound: 'default',
+        category: 'REACT_NATIVE'
+      },
+    });
+  }
+
+  _onNotification(notification) {
+    AlertIOS.alert(
+      'Notification Received',
+      'Alert message: ' + notification.getMessage(),
+      [{
+        text: 'Dismiss',
+        onPress: null,
+      }]
+    );
+  }
+}
+
+class NotificationPermissionExample extends React.Component {
+  state: any;
+
+  constructor(props) {
+    super(props);
+    this.state = {permissions: null};
+  }
+
+  render() {
+    return (
+      <View>
+        <Button
+          onPress={this._showPermissions.bind(this)}
+          label="Show enabled permissions"
+        />
+        <Text>
+          {JSON.stringify(this.state.permissions)}
+        </Text>
+      </View>
+    );
+  }
+
+  _showPermissions() {
+    PushNotificationIOS.checkPermissions((permissions) => {
+      this.setState({permissions});
+    });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  button: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonLabel: {
+    color: 'blue',
+  },
+});
+
+exports.title = 'PushNotificationIOS';
+exports.description = 'Apple PushNotification and badge value';
+exports.examples = [
+{
+  title: 'Badge Number',
+  render(): ReactElement {
+    PushNotificationIOS.requestPermissions();
+
+    return (
+      <View>
+        <Button
+          onPress={() => PushNotificationIOS.setApplicationIconBadgeNumber(42)}
+          label="Set app's icon badge to 42"
+        />
+        <Button
+          onPress={() => PushNotificationIOS.setApplicationIconBadgeNumber(0)}
+          label="Clear app's icon badge"
+        />
+      </View>
+    );
+  },
+},
+{
+  title: 'Push Notifications',
+  render(): ReactElement {
+    return <iosPush />;
+  }
+},
+{
+  title: 'Notifications Permissions',
+  render(): ReactElement {
+    return <NotificationPermissionExample />;
+  }
+}];
+
+AppRegistry.registerComponent('iosPush2', () => iosPush2);
